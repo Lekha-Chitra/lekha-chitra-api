f30260c242d8c68575cb7de11d115c845f958f48
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-02 13:14:13 +0545

    feat: Adds JWT Generator

---
+++ LekhaChitra.Application/Helpers/JwtHelper/IJwtService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Application.Helpers.JwtHelper
+{
+    public interface IJwtService
+    {
+        Task<string> GenerateNewJsonWebToken(List<Claim> claims);
+    }
+}
