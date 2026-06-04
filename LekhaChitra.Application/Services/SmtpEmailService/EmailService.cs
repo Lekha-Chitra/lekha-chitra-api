6fbe13b003772eef0d43e0daa0e5c640d7dbb634
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-04 16:19:30 +0545

    feat: email sender updated for scalability

---
+++ LekhaChitra.Application/Services/SmtpEmailService/EmailService.cs
@@ -0,0 +1,54 @@
+﻿using LekhaChitra.Application.DTO;
+using LekhaChitra.Application.Helpers.InMemoryDb.EmailDb;
+using LekhaChitra.Application.Interfaces.SmtpEmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Cryptography;
+
+namespace LekhaChitra.Application.Services.SmtpEmail
+{
+    public class EmailService : IEmailService
+    {
+        private readonly SmtpEmailSettingDTO _settings;
+       
+
+        public EmailService(IOptions<SmtpEmailSettingDTO> options)
+        {
+            _settings = options.Value;
+        }
+
+        public string GenerateOtp()
+        {
+            return RandomNumberGenerator
+                  .GetInt32(100000, 1000000)
+                  .ToString();
+        }
+
+        public async Task SendAsync(string to, string subject, string body)
+        {
+            using var client = new SmtpClient(_settings.Host)
+            {
+                Port = _settings.Port,
+                Credentials = new NetworkCredential(_settings.User, _settings.Password),
+                EnableSsl = _settings.EnableSsl
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress(_settings.From),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+
+            };
+
+            mail.To.Add(to);
+
+            await client.SendMailAsync(mail);
+
+          
+        }
+    }
+}
