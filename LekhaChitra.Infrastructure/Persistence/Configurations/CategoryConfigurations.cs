de749c81f6413c31f9b961b249d69b4293e1636e
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-11 14:37:40 +0545

    Feat: Adds Fluent Validation for Category and SubCategory

---
+++ LekhaChitra.Infrastructure/Persistence/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,32 @@
+﻿using LekhaChitra.Domain.Entities.Application.Categories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // for table name
+            builder.ToTable("Categories");
+            builder.HasKey(c => c.Id);
+
+            // column properties
+            builder.Property(x => x.Name)
+                     .IsRequired()
+                     .HasMaxLength(100);
+
+            // 1:M relationship with SubCategory
+            builder.HasMany(x => x.SubCategories)
+                     .WithOne(x => x.Category)
+                     .HasForeignKey(x => x.CategoryId)
+                     .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
