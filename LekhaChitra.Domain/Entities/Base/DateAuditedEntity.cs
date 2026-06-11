5968b0a768d3a6fbfba2e615036b7c53e2196570
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-11 14:19:56 +0545

    Feat: Adds Base entities for audit logs

---
+++ LekhaChitra.Domain/Entities/Base/DateAuditedEntity.cs
@@ -0,0 +1,15 @@
+﻿using LekhaChitra.Domain.Interface.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Domain.Entities.Base
+{
+    public abstract class DateAuditedEntity<TPrimaryKey> : Entity<TPrimaryKey>, IDateAudited
+    {
+        public DateTime? AddedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
