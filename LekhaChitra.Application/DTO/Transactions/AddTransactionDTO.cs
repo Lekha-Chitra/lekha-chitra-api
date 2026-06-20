dc6231c457de00ad8d27504aec890e63ee5df645
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-20 14:27:14 +0545

    Feat: Adds add-Transaction Command and Handler

---
+++ LekhaChitra.Application/DTO/Transactions/AddTransactionDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Application.DTO.Transactions
+{
+    public class AddTransactionDTO
+    {
+        public string TransactionType { get; set; }
+        public string PaymentType { get; set; }
+        public string TO { get; set; }
+        public string From { get; set; }
+        public string ClientName { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Remarks { get; set; }
+
+    }
+}
