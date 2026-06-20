eb6ecc1de5a406d201854053ec6598ab93bf830d
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-20 14:26:36 +0545

    Feat: Adds enums for payments and transactions

---
+++ LekhaChitra.Application/Constants/Enums/PaymentMethodEnums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Application.Constants.Enums
+{
+    public enum PaymentMethodEnums
+    {
+        Online = 0,
+        Cash = 1,
+        Loan = 2,
+    }
+}
---
+++ LekhaChitra.Application/Constants/Enums/PaymentStatusEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Application.Constants.Enums
+{
+    public enum PaymentStatusEnum
+    {
+        Pending = 0,
+        Completed = 1,
+        Failed = 2,
+        Refunded = 3,
+        Cancelled = 4
+    }
+}
