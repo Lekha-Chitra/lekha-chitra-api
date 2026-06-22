5cda1c37ce2d41c116b025bd9efd3ed4606ddbd8
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-22 13:50:27 +0545

    Feat: Adds filter client

---
+++ LekhaChitra.Application/Features/Clients/FilterClients/FilterClientQuery.cs
@@ -0,0 +1,13 @@
+﻿using LekhaChitra.Application.DTO.Clients;
+using LekhaChitra.Application.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LekhaChitra.Application.Features.Clients.FilterClients
+{
+    public record FilterClientQuery(GetClientFilter Filter) : IRequest<ServiceResponse>;
+}
