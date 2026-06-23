6cfbb59f368a3cf500127a685ab45d545a674b96
[rose3085] <[rabinasedhai1@gmail.com]>
2026-06-23 13:42:26 +0545

    Fixs: Migration updated

---
+++ LekhaChitra.Infrastructure/Migrations/20260623075617_latestPaymentMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LekhaChitra.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class latestPaymentMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Payments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Payments",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
