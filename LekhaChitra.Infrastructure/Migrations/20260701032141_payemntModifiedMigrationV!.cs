ab311d3639d27ec5fc4e6f64bca8e4edc01ce466
[rose3085] <[rabinasedhai1@gmail.com]>
2026-07-01 09:08:21 +0545

    Fixs: Updated dbContext snapshot for new index

---
+++ LekhaChitra.Infrastructure/Migrations/20260701032141_payemntModifiedMigrationV!.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LekhaChitra.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class payemntModifiedMigrationV : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_AddedDate",
+                table: "Transactions",
+                column: "AddedDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_AddedDate",
+                table: "Payments",
+                column: "AddedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_AddedDate",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_AddedDate",
+                table: "Payments");
+        }
+    }
+}
